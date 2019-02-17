4e49f4e93b59cdbcbaddbf245206348c5dfa5f28
Axel Seemann <kummeraxel@gmail.com>
2019-02-17 10:33:48 +0100

    Change adding LogWriter. This fixes an issue which overwrites other defined logwirter if the extension is activated

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,14 +8,26 @@ if(!defined('TYPO3_MODE')){
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'] = array(
-    // configuration for DEBUG level log entries
-    \TYPO3\CMS\Core\Log\LogLevel::DEBUG => array(
-        // add a FileWriter
-        \AxelKummer\AkuLogbook\LogWriter::class => array()
-    )
+/**
+ * Add writer to TYPO3
+ */
+$writerConfiguration = array(
+    // add a LogBook Writer
+    \AxelKummer\AkuLogbook\LogWriter::class => array()
 );
 
+if (false === is_array($GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::DEBUG])) {
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::DEBUG] = [];
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::DEBUG] = array_merge(
+    $writerConfiguration,
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['writerConfiguration'][\TYPO3\CMS\Core\Log\LogLevel::DEBUG]
+);
+
+/**
+ * Register DevLogger
+ */
 if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_div.php']['devLog'])) {
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_div.php']['devLog'] = [];
 }
